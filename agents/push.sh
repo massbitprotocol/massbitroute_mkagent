b26a7f032faa7331a867083f97508d755f0c1db9
Vu Tran <baysao@gmail.com>
2022-04-27 14:41:50 +0700

    Update add custom hosts file

--- agents/push.sh
+++ agents/push.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
+
 # type=monitor
 SITE_ROOT=$1
+if [ -f "$SITE_ROOT/.env_raw" ]; then source $SITE_ROOT/.env_raw; fi
+
 # if [ -z "$SITE_ROOT" ]; then
 # 	SITE_ROOT=/massbit/massbitroute/app/src/sites/services/$type
 # fi
 
 # dir=$SITE_ROOT/etc/mkagent/agents
 dir=$(dirname $(realpath $0))
-pip="pip install "
+# pip="pip install "
 cd $dir
 
-if [ ! -f "/usr/bin/python3" ]; then
-	apt install -y python3
-fi
+# if [ ! -f "/usr/bin/python3" ]; then
+# 	apt install -y python3
+# fi
 
-if [ ! -f "/usr/bin/pip" ]; then
-	apt install -y python3-pip
-fi
+# if [ ! -f "/usr/bin/pip" ]; then
+# 	apt install -y python3-pip
+# fi
 
-export URL=https://monitor.mbr.massbitroute.com
+export URL=https://monitor.mbr.$DOMAIN
+export PORTAL_DOMAIN=https://dapi.$DOMAIN
+
+tmp=$(mktemp)
+curl -sSfL https://$PORTAL_DOMAIN/deploy/hosts -o $tmp >/dev/nul
+
+while read _ip _host; do
+	echo $_ip $_host
+	grep $_host /etc/hosts >/dev/null
+	if [ $? -ne 0 ]; then
+		echo $_ip $_host >>/etc/hosts
+	fi
+done <$tmp
+rm $tmp
 # export CHECK_MK_AGENT=$dir/check_mk_agent.linux
 export CHECK_MK_AGENT=$dir/check_mk_caching_agent.linux
 
@@ -44,8 +60,8 @@ _kill)
 		TK="${TYPE}-${BLOCKCHAIN}-${NETWORK}-${ID}"
 	fi
 	export TOKEN=$(echo -n ${TK} | sha1sum | cut -d' ' -f1)
-	$pip --upgrade pip
-	$pip -r requirements.txt
+	# $pip --upgrade pip
+	# $pip -r requirements.txt
 	python3 $dir/push.py
 	;;
 
