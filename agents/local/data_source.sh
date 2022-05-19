9ea6466e6c5294ac4b82987e94e67215d936d20e
Vu Tran <baysao@gmail.com>
2022-05-19 14:23:36 +0700

    Update local dir for mkagent

--- agents/local/data_source.sh
+++ agents/local/data_source.sh
@@ -9,10 +9,22 @@ if [ -f "$_data_uri_f" ]; then
 	_data_uri=$(cat $_data_uri_f | sed 's/ //g')
 	_scheme=$(echo $_data_uri | awk -F[/:] '{print $1}')
 	_ssl_opt=""
+
+	_port=80
 	if [ "$_scheme" == "https" ]; then
 		_ssl_opt="--ssl"
+		_port=443
 	fi
-	_hostname=$(echo $_data_uri | awk -F[/:] '{print $4}')
+
+	_hostname1=$(echo $_data_uri | awk -F'/' '{print $3}')
+	_hostname=$(echo $_hostname1 | awk -F':' '{print $1}')
+	_port=$(echo $_hostname1 | awk -F':' '{print $2}')
+	_port_opt=""
+
+	if [ -n "$_port" ]; then
+		_port_opt="-p $_port"
+	fi
+
 	_checkname="mbr-datasource-$_data_uri"
 	$check_http -H $_hostname -u $_data_uri -T application/json --method=POST --post='{"id": "blockNumber", "jsonrpc": "2.0", "method": "eth_getBlockByNumber", "params": ["latest", false]}' -t 3 $_ssl_opt | tail -1 |
 		awk -F'|' -v checkname=$_checkname '{st=0;perf="-";if(index($1,"CRITICAL") != 0){st=2} else if(index($1,"WARNING") != 0){st=1} else {gsub(/ /,"|",$2);perf=$2;};print st,checkname,perf,$1}'
