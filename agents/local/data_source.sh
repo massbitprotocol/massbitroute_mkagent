02a322bcc7fd7537e2472c2db0faeb46c97819b6
Vu Tran <baysao@gmail.com>
2022-05-19 17:00:31 +0700

    Update add checker for gateway to nodes

--- agents/local/data_source.sh
+++ agents/local/data_source.sh
@@ -4,8 +4,15 @@ if [ ! -f "$check_http" ]; then
 	apt install -y monitoring-plugins
 fi
 
+_post_data='{"id": "blockNumber", "jsonrpc": "2.0", "method": "eth_getBlockByNumber", "params": ["latest", false]}'
 _data_uri_f="/massbit/massbitroute/app/src/sites/services/node/vars/DATA_URI"
+
 if [ -f "$_data_uri_f" ]; then
+	_blockchain="/massbit/massbitroute/app/src/sites/services/node/vars/BLOCKCHAIN"
+	if [ "$_blockchain" == "dot" ]; then
+		_post_data='{"jsonrpc":"2.0","method":"chain_getBlock","params": [],"id": 1}'
+	fi
+
 	_data_uri=$(cat $_data_uri_f | sed 's/ //g')
 	_scheme=$(echo $_data_uri | awk -F[/:] '{print $1}')
 	_ssl_opt=""
@@ -32,6 +39,6 @@ if [ -f "$_data_uri_f" ]; then
 	fi
 
 	_checkname="mbr-datasource-$_data_uri"
-	$check_http -H $_hostname -u $_path -T application/json --method=POST --post='{"id": "blockNumber", "jsonrpc": "2.0", "method": "eth_getBlockByNumber", "params": ["latest", false]}' -t 3 $_ssl_opt $_port_opt | tail -1 |
+	$check_http -H $_hostname -u $_path -T application/json --method=POST --post=$_post_data -t 3 $_ssl_opt $_port_opt | tail -1 |
 		awk -F'|' -v checkname=$_checkname '{st=0;perf="-";if(index($1,"CRITICAL") != 0){st=2} else if(index($1,"WARNING") != 0){st=1} else {gsub(/ /,"|",$2);perf=$2;};print st,checkname,perf,$1}'
 fi
--- agents/local/gateway_check_nodes.sh
+++ agents/local/gateway_check_nodes.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 _nodes=/massbit/massbitroute/app/src/sites/services/gateway/http.d/gw-dot-mainnet-nodes.conf
 check_http="/usr/lib/nagios/plugins/check_http"
+_post_data='{"id": "blockNumber", "jsonrpc": "2.0", "method": "eth_getBlockByNumber", "params": ["latest", false]}'
 _http() {
 	_hostname=$1
 	_id=$(echo $_hostname | cut -d'.' -f1)
@@ -9,10 +10,15 @@ _http() {
 	_path=$4
 	_token=$5
 	_checkname="mbr-node-$_id"
-	$check_http -H $_hostname -k "x-api-key: $_token" -u $_path -T application/json --method=POST --post='{"id": "blockNumber", "jsonrpc": "2.0", "method": "eth_getBlockByNumber", "params": ["latest", false]}' -t 3 --ssl -p $_port | tail -1 |
+	$check_http -H $_hostname -k "x-api-key: $_token" -u $_path -T application/json --method=POST --post=$_post_data -t 3 --ssl -p $_port | tail -1 |
 		awk -F'|' -v checkname=$_checkname '{st=0;perf="-";if(index($1,"CRITICAL") != 0){st=2} else if(index($1,"WARNING") != 0){st=1} else {gsub(/ /,"|",$2);perf=$2;};print st,checkname,perf,$1}'
 }
 if [ -f "$_nodes" ]; then
+	_blockchain="/massbit/massbitroute/app/src/sites/services/gateway/vars/BLOCKCHAIN"
+	if [ "$_blockchain" == "dot" ]; then
+		_post_data='{"jsonrpc":"2.0","method":"chain_getBlock","params": [],"id": 1}'
+	fi
+
 	awk -f /massbit/massbitroute/app/src/sites/services/mkagent/agents/extract_nodes.awk /massbit/massbitroute/app/src/sites/services/gateway/http.d/gw-dot-mainnet-nodes.conf | while read _token _domain _url; do
 		_path="/"
 		_ip=$(echo $_url | cut -d'/' -f3)
