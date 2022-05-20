a0ee274ce0bd270963709fe3f2d026b69be8782c
Vu Tran <baysao@gmail.com>
2022-05-20 11:41:18 +0700

    Add cmd_server check

--- agents/local/cmd_server_status.sh
+++ agents/local/cmd_server_status.sh
@@ -2,7 +2,7 @@
 _type=$(supervisorctl status | awk '$1 !~ /_monitor/ && $1 ~ /^mbr_/{gsub(/^mbr_/,"",$1);print $1}')
 _cmd=/massbit/massbitroute/app/src/sites/services/${_type}/cmd_server
 tmp=$(mktemp)
-_cmd status | awk '$3 ~ /pid/' >$tmp
+$_cmd status | awk '$3 ~ /pid/' >$tmp
 n=$(wc -l $tmp | cut -d' ' -f1)
 grep -v RUNNING $tmp >${tmp}.1
 st=0
