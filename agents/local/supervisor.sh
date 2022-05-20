39ed609f1028395571cd225349c972a2c209827f
Vu Tran <baysao@gmail.com>
2022-05-20 11:35:47 +0700

    Add supervisorctl check

--- agents/local/supervisor.sh
+++ agents/local/supervisor.sh
@@ -4,7 +4,7 @@ supervisorctl status >$tmp
 n=$(wc -l $tmp | cut -d' ' -f1)
 grep -v RUNNING $tmp >${tmp}.1
 st=0
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
 	st=2
 fi
 echo "$st mbr-supervisor $n $(cat ${tmp}.1)"
