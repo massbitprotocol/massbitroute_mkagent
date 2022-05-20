1506c5eea8a8d47708a2fa201be9ade1bfdb300a
Vu Tran <baysao@gmail.com>
2022-05-20 11:40:39 +0700

    Add cmd_server check

---
+++ agents/local/cmd_server_status.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+_type=$(supervisorctl status | awk '$1 !~ /_monitor/ && $1 ~ /^mbr_/{gsub(/^mbr_/,"",$1);print $1}')
+_cmd=/massbit/massbitroute/app/src/sites/services/${_type}/cmd_server
+tmp=$(mktemp)
+_cmd status | awk '$3 ~ /pid/' >$tmp
+n=$(wc -l $tmp | cut -d' ' -f1)
+grep -v RUNNING $tmp >${tmp}.1
+st=0
+if [ $? -ne 0 ]; then
+	st=2
+fi
+echo "$st mbr-cmd_server $n $(cat ${tmp}.1)"
+rm $tmp*
