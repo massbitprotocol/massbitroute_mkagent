acbc0707e33496eb5665bc65bf2969ffba59321d
Vu Tran <baysao@gmail.com>
2022-05-20 16:59:47 +0700

    Update cached ip values

--- agents/local/check_firewall.sh
+++ agents/local/check_firewall.sh
@@ -11,7 +11,17 @@ fi
 
 curl="/usr/bin/curl -sk"
 #ip=$($curl https://internal.monitor.mbr.massbitroute.net/__my/ip)
-ip=$($curl http://ipv4.icanhazip.com)
+
+cache_ip_file=/tmp/ip
+if [ -f "$cache_ip_file" ]; then
+	ip=$(cat $cache_ip_file)
+	if [ -z "$ip" ]; then
+		ip=$($curl http://ipv4.icanhazip.com)
+		echo $ip >$cache_ip_file
+	fi
+
+fi
+
 is_open=$($curl https://internal.monitor.mbr.massbitroute.net/__check/port/$ip/tcp/443)
 if [ $is_open -eq 0 ]; then
 	echo $is_open mbr-firewall-443 - "Port 443 is open" >$cache_file
