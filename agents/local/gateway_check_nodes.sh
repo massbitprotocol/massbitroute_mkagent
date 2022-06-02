e16a5e252d1f357005365c58deb739811e851611
Vu Tran <baysao@gmail.com>
2022-06-02 16:17:21 +0700

    Fix test_speed in 5 seconds

--- agents/local/gateway_check_nodes.sh
+++ agents/local/gateway_check_nodes.sh
@@ -94,10 +94,10 @@ _test_speed() {
 		return
 	fi
 	tmp=$(mktemp)
-	timeout 3 wget -O $tmp --no-check-certificate https://$_ip/__log/128M
+	timeout 5 wget -O $tmp --no-check-certificate https://$_ip/__log/128M
 	_size=$(stat --printf="%s" $tmp)
 	if [ $_size -gt 0 ]; then
-		_speed=$(expr $_size / 2 / 1024)
+		_speed=$(expr $_size / 4 / 1024)
 		echo "0 mbr-node-speed-${_id} speed=$_speed speed is $_speed KB/s" >$_ff
 		cat $_ff
 	fi
