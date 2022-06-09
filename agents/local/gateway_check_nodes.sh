952d2ced96a6978578664a2c20d5f482508f30ce
Vu Tran <baysao@gmail.com>
2022-06-09 14:32:15 +0700

    Add info about geo in message check

--- agents/local/gateway_check_nodes.sh
+++ agents/local/gateway_check_nodes.sh
@@ -201,9 +201,10 @@ _node_check_geo() {
 			_path_ping="/_ping"
 			_port=443
 			_domain="${_id}.node.mbr.$DOMAIN"
-			_http $_domain $_ip $_port $_path $_token $_blockchain mbr-node-$_id POST geo={_continent}-${_country}
-			_http $_ip $_ip $_port $_path_ping $_token $_blockchain mbr-node-${_id}-ping GET geo={_continent}-${_country}
-			_test_speed $_ip $_id geo={_continent}-${_country}
+			_info="geo={_continent}-${_country}"
+			_http $_domain $_ip $_port $_path $_token $_blockchain mbr-node-$_id POST $_info
+			_http $_ip $_ip $_port $_path_ping $_token $_blockchain mbr-node-${_id}-ping GET $_info
+			_test_speed $_ip $_id $_info
 		done
 	done
 }
