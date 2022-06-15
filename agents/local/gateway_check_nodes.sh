3942e95430ca0f6be403cde7d7eca325ab70589f
Vu Tran <baysao@gmail.com>
2022-06-15 13:21:30 +0700

    Add internal dns check

--- agents/local/gateway_check_nodes.sh
+++ agents/local/gateway_check_nodes.sh
@@ -150,7 +150,9 @@ _http_api() {
 	if [ -n "$_domain" ]; then
 		_http_api_check $_domain $_path
 
-		_checkname="dns_8.8.8.8"
+		_h=$(cat /etc/resolv.conf | awk '/nameserver/{print $2}')
+		_checkname="dns_$h"
+		$check_dns -H $_domain | awk -F'|' -v checkname=$_checkname -v msg="$_msg" '{st=0;perf="-";if(index($1,"CRITICAL") != 0){st=2} else if(index($1,"WARNING") != 0){st=1} else {gsub(/ /,"|",$2);perf=$2;};print st,checkname,perf,$1,msg}'
 		$check_dns -H $_domain -s 8.8.8.8 | awk -F'|' -v checkname=$_checkname -v msg="$_msg" '{st=0;perf="-";if(index($1,"CRITICAL") != 0){st=2} else if(index($1,"WARNING") != 0){st=1} else {gsub(/ /,"|",$2);perf=$2;};print st,checkname,perf,$1,msg}'
 		_checkname="dns_ns1"
 		$check_dns -H $_domain -s ns1.$DOMAIN | awk -F'|' -v checkname=$_checkname -v msg="$_msg" '{st=0;perf="-";if(index($1,"CRITICAL") != 0){st=2} else if(index($1,"WARNING") != 0){st=1} else {gsub(/ /,"|",$2);perf=$2;};print st,checkname,perf,$1,msg}'
