b79d0277adeef97dddd43994280027535f90d4bb
Vu Tran <baysao@gmail.com>
2022-06-23 17:16:35 +0700

    Fix service check is empty process

--- agents/local/services.sh
+++ agents/local/services.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-nginx=$(ps -ef | awk '/nginx: master process/ {gsub(/^.*nginx: master process/,"");print}' | head -1)
-msg=$($nginx -t 2>&1)
+nginx="$(ps -ef | awk '/nginx: master process/ {gsub(/^.*nginx: master process/,"");print}' | grep -v print | head -1)"
+if [ -z "$nginx" ]; then
+	echo $st nginx_check - "nginx process not found"
+	exit 1
+fi
+
+msg="$($nginx -t 2>&1)"
 st=$?
-echo $st nginx_check - $msg
+echo $st nginx_check - "$msg"
