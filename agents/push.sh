19e9246ccaf1a556e1579ad3b0b7293d662edc99
Vu Tran <baysao@gmail.com>
2022-10-20 16:39:32 +0700

    add loop

--- agents/push.sh
+++ agents/push.sh
@@ -30,64 +30,71 @@ if [ -d "$state_dir" ]; then
 	rm -rf $state_dir
 fi
 
-_update_local_check() {
+loop() {
 	while true; do
-		echo "$date" >>$log_local_check
-		_t1=$(date +%s)
-		find $dir/local -type f -iname '*.sh' | while read cmd; do
-			echo bash $cmd 1 >>$log_local_check
-			timeout 300 bash $cmd 1 >>$log_local_check
-		done
-		_t2=$(date +%s)
-		_t=$(expr $_t2 - $_t1)
-		echo "0 local_check_time t=$_t time run $_t seconds" >$state_dir
+		$0 $@
 		sleep 30
 	done
+
+}
+_update_local_check() {
+	# while true; do
+	echo "$date" >>$log_local_check
+	_t1=$(date +%s)
+	find $dir/local -type f -iname '*.sh' | while read cmd; do
+		echo bash $cmd 1 >>$log_local_check
+		timeout 300 bash $cmd 1 >>$log_local_check
+	done
+	_t2=$(date +%s)
+	_t=$(expr $_t2 - $_t1)
+	echo "0 local_check_time t=$_t time run $_t seconds" >$state_dir
+	# 	sleep 30
+	# done
 }
 _push() {
-	while true; do
-		if [ -z "$DOMAIN" ]; then
-			echo "DOMAIN not set"
-			exit 1
-		fi
+	# while true; do
+	if [ -z "$DOMAIN" ]; then
+		echo "DOMAIN not set"
+		exit 1
+	fi
 
-		if [ -z "$MONITOR_SCHEME" ]; then
-			echo "MONITOR_SCHEME not set"
-			exit 1
-		fi
+	if [ -z "$MONITOR_SCHEME" ]; then
+		echo "MONITOR_SCHEME not set"
+		exit 1
+	fi
 
-		export CHECK_MK_AGENT=$dir/check_mk_agent.linux
+	export CHECK_MK_AGENT=$dir/check_mk_agent.linux
 
-		TYPE=$(cat $SITE_ROOT/vars/TYPE)
-		if [ ! -f "$SITE_ROOT/vars/ID" ]; then
-			echo 1 >$SITE_ROOT/vars/ID
-		fi
+	TYPE=$(cat $SITE_ROOT/vars/TYPE)
+	if [ ! -f "$SITE_ROOT/vars/ID" ]; then
+		echo 1 >$SITE_ROOT/vars/ID
+	fi
 
-		ID=$(cat $SITE_ROOT/vars/ID)
-		if [ -z "$ID" ]; then exit 1; fi
+	ID=$(cat $SITE_ROOT/vars/ID)
+	if [ -z "$ID" ]; then exit 1; fi
 
-		if [ \( "$TYPE" = "gateway" \) -o \( "$TYPE" = "node" \) ]; then
-			export BLOCKCHAIN=$(cat $SITE_ROOT/vars/BLOCKCHAIN)
-			export NETWORK=$(cat $SITE_ROOT/vars/NETWORK)
-			export URL=$MONITOR_SCHEME://${TYPE}-${BLOCKCHAIN}-${NETWORK}.monitor.mbr.$DOMAIN
-			TK="${TYPE}-${BLOCKCHAIN}-${NETWORK}-${ID}"
-		else
-			ID=$HOSTNAME
-			TK="${TYPE}-${ID}"
-			export URL=$MONITOR_SCHEME://internal.monitor.mbr.$DOMAIN
-		fi
-		export TOKEN=$(echo -n ${TK} | sha1sum | cut -d' ' -f1)
+	if [ \( "$TYPE" = "gateway" \) -o \( "$TYPE" = "node" \) ]; then
+		export BLOCKCHAIN=$(cat $SITE_ROOT/vars/BLOCKCHAIN)
+		export NETWORK=$(cat $SITE_ROOT/vars/NETWORK)
+		export URL=$MONITOR_SCHEME://${TYPE}-${BLOCKCHAIN}-${NETWORK}.monitor.mbr.$DOMAIN
+		TK="${TYPE}-${BLOCKCHAIN}-${NETWORK}-${ID}"
+	else
+		ID=$HOSTNAME
+		TK="${TYPE}-${ID}"
+		export URL=$MONITOR_SCHEME://internal.monitor.mbr.$DOMAIN
+	fi
+	export TOKEN=$(echo -n ${TK} | sha1sum | cut -d' ' -f1)
 
-		export PUSH_URL=push
-		/usr/bin/python3 push.py >>$log_push
-		sleep 30
-	done
+	export PUSH_URL=push
+	/usr/bin/python3 push.py >>$log_push
+	# 	sleep 30
+	# done
 
 }
 if [ $# -eq 0 ]; then
 	(
-		echo "$sc $SITE_ROOT _push"
-		echo "$sc $SITE_ROOT _update_local_check"
+		echo "$sc $SITE_ROOT loop _push"
+		echo "$sc $SITE_ROOT loop _update_local_check"
 	) | parallel -j2
 else
 	$@
